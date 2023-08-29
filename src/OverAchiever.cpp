736cef89efb35dcec6760060e3d839d21a409288
AnchyDev <lewienato@gmail.com>
2023-08-29 17:43:01 +1000

    Added config & reward based on achievement count.

--- src/OverAchiever.cpp
+++ src/OverAchiever.cpp
@@ -1,5 +1,117 @@
 #include "OverAchiever.h"
 
+void OverAchieverPlayerScript::OnLogin(Player* player)
+{
+    if (!player)
+    {
+        return;
+    }
+
+    auto points = GetAchievementPointsFromDB(player);
+    UpdatePointsForPlayer(player, points);
+}
+
+void OverAchieverPlayerScript::OnUpdate(Player* player, uint32 p_time)
+{
+    if (!sConfigMgr->GetOption<bool>("OverAchiever.Enabled", false))
+    {
+        return;
+    }
+
+    auto checkFrequency = sConfigMgr->GetOption<uint32>("OverAchiever.RewardFrequencySeconds", 30);
+    currentFrequencyMS += p_time;
+
+    if ((currentFrequencyMS / 1000) < checkFrequency)
+    {
+        return;
+    }
+
+    currentFrequencyMS = 0;
+
+    if (!player)
+    {
+        return;
+    }
+
+    uint32 points = GetPointsForPlayer(player);
+    float constant = 100;
+    float multi = 0.1;
+    float amount = 1 + points / constant * multi;
+    player->AddItem(37711, amount);
+
+    ChatHandler(player->GetSession()).SendSysMessage(Acore::StringFormatFmt("Rewarding amount: {}", amount));
+}
+
+uint32 OverAchieverPlayerScript::GetAchievementPointsFromDB(Player* player)
+{
+    if (!player)
+    {
+        return 0;
+    }
+
+    try
+    {
+        std::vector<uint32> achievements;
+        QueryResult qResult = CharacterDatabase.Query("SELECT achievement FROM character_achievement WHERE guid = {}", player->GetGUID().GetRawValue());
+
+        do
+        {
+            Field* fields = qResult->Fetch();
+            achievements.push_back(fields[0].Get<uint32>());
+            
+        } while (qResult->NextRow());
+
+        return achievements.size();
+    }
+    catch (std::exception ex)
+    {
+        LOG_INFO("module", "Failed to load achievements from DB for player {}: {}", player->GetName(), ex.what());
+        return 0;
+    }
+
+    return 0;
+}
+
+void OverAchieverPlayerScript::UpdatePointsForPlayer(Player* player, uint32 points)
+{
+    if (!player)
+    {
+        return;
+    }
+
+    auto guid = player->GetGUID().GetRawValue();
+    auto ptr = achievementPoints.find(guid);
+
+    if (ptr == achievementPoints.end())
+    {
+        achievementPoints.emplace(guid, points);
+    }
+    else
+    {
+        ptr->second = points;
+    }
+}
+
+uint32 OverAchieverPlayerScript::GetPointsForPlayer(Player* player)
+{
+    if (!player)
+    {
+        return 0;
+    }
+
+    auto guid = player->GetGUID().GetRawValue();
+    auto ptr = achievementPoints.find(guid);
+
+    if (ptr == achievementPoints.end())
+    {
+        return 0;
+    }
+    else
+    {
+        return ptr->second;
+    }
+}
+
 void SC_AddOverAchieverScripts()
 {
     new OverAchieverPlayerScript();
--- src/OverAchiever.h
+++ src/OverAchiever.h
@@ -1,8 +1,32 @@
 #ifndef MODULE_OVERACHIEVER_H
 #define MODULE_OVERACHIEVER_H
 
-class OverAchieverPlayerScript {
+#include "Config.h"
+#include "Chat.h"
+#include "CharacterDatabase.h"
+#include "Player.h"
+#include "ScriptMgr.h"
 
+#include <unordered_map>
+#include <vector>
+
+class OverAchieverPlayerScript : PlayerScript {
+public:
+    OverAchieverPlayerScript() : PlayerScript("OverAchieverPlayerScript")
+    {
+        currentFrequencyMS = 0;
+    }
+
+private:
+    void OnLogin(Player* /*player*/) override;
+    void OnUpdate(Player* /*player*/, uint32 /*p_time*/) override;
+
+    uint32 GetAchievementPointsFromDB(Player* /*player*/);
+    void UpdatePointsForPlayer(Player* /*player*/, uint32 /*points*/);
+    uint32 GetPointsForPlayer(Player* /*player*/);
+
+    uint32 currentFrequencyMS;
+    std::unordered_map<uint64, uint32> achievementPoints;
 };
 
 #endif // MODULE_OVERACHIEVER_H
